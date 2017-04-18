87aef4c9cf95189125523c05555ba0657e9cf4b1
Sasha Lishchynskyy <sasha.lishchynskyy@gmail.com>
2017-04-18 15:33:06 +0300

    CyberBeatBox (2.1) MiniMusicPlayer2
    Program creates sequence and track with bunch of events to make the
    notes keep going up (from piano note 5 to piano note 61) and each time
    when get the event print "la" to the command-line.

--- src/MiniMusicPlayer2.java
+++ src/MiniMusicPlayer2.java
@@ -1,16 +1,25 @@
 import javax.sound.midi.*;
 
-public class MiniMusicPlayer1 {
+public class MiniMusicPlayer2 implements ControllerEventListener{
     public static void main(String[] args) {
+        MiniMusicPlayer2 mini = new MiniMusicPlayer2();
+        mini.go();
+    }
+
+    public void go(){
         try {
             Sequencer sequencer = MidiSystem.getSequencer();   //make a Sequencer
             sequencer.open();                                  //and open
+
+            int[] eventsIWant = {127};
+            sequencer.addControllerEventListener(this, eventsIWant);
 
             Sequence seq = new Sequence(Sequence.PPQ, 4);   //make a sequence
             Track track = seq.createTrack();                          //make a track
 
             for (int i = 5; i < 61; i += 2) { //the notes keep going up (from piano note 5 to piano note 61)
                 track.add(makeEvent(144, 1, i, 100, i));
+                track.add(makeEvent(176,1,127,0, i));
                 track.add(makeEvent(128, 1, i, 100, i + 2));
             }
 
@@ -31,5 +40,9 @@ public class MiniMusicPlayer1 {
         } catch (Exception e) {
         }
         return event; //return event (MidiEvent)
+    }
+
+    public void controlChange(ShortMessage event) {
+        System.out.println("la");           //each time we get the event, we'll print "la" to the command-line
     }
 }
